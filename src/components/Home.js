e87ba6fa69dbd727b9522d2680b079e16b615a0e
Onowoni <danielonowoni1@gmail.com>
2023-01-04 17:13:32 +0100

    added github link

--- src/components/Home.js
+++ src/components/Home.js
@@ -54,7 +54,7 @@ export default function Home() {
                 <Textdiv>
                     <p>Hi there! My name is <span className="large-text">Daniel</span> and I am a backend developer with a passion for creating efficient and scalable web applications. I have developed a strong skill set in languages such as Python and JavaScript, as well as backend technologies like Django and Flask.</p>
                     <p>I love the challenge of designing and developing <span className="large-text">Web applications</span> that are both efficient and user-friendly. I've worked with all sorts of APIs, third-party integrations, and <span className="large-text">Databases</span>, and I'm always looking for new ways to improve my skills and stay up-to-date with the latest technologies.</p>
-                    <p>In my free time, you can usually find me tinkering with personal projects or <span className="large-text">Nerding</span> out over the latest tech news. I'm always down to chat about programming or just hang out, so don't be shy! Feel free to shoot me a message or connect with me on <a href="https://linkedin.com/in/daniel-onowoni" className="text-link">LinkedIn.</a></p>
+                    <p>In my free time, you can usually find me tinkering with personal <span className="large-text">projects</span> or Nerding out over the latest tech news. I'm always down to chat about programming or just hang out, so don't be shy! Feel free to shoot me a message or connect with me on <a href="https://linkedin.com/in/daniel-onowoni" className="text-link">LinkedIn.</a></p>
                 </Textdiv>
                 <img src={star1} alt="logo" className="star spinning" />
                 <Kunai src={kunai} alt="logo" className="kunai spinning" />
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,5 @@
 import img from '../img/djurnal.png'
 import poolinc from '../img/poolinc.png'
-import poolinc2 from '../img/secondpoolinc.png'
 import cv from "../img/DanielOnowoni.pdf";
 import { Fullpage, FullPageSections, FullpageSection, FullpageNavigation } from '@ap.cx/react-fullpage'
 
@@ -18,39 +17,45 @@ export default function Work() {
                             <img src={img} alt="Home screen of project" />
                         </div>
                         <div className="project-text">
-                            <h1>Djurnal</h1>
-                            <p>this is an app Made with the python Flask framework that help users in documenting their day,
-                                It has basic CRUD funtionalities as well as a login system that uses cookies to remember users
+                            <h1>Poultry Plus</h1>
+                            <p>Introducing the ultimate tool for livestock owners! Our site makes it easy to keep track of your animals, and stay on top of important tasks like feeding and vaccination schedules. Plus,
+                                our built-in bookkeeping features allow you to easily track your expenses and income, so you can keep your farm or ranch running smoothly. With our user-friendly interface and powerful features,
+                                you'll have everything you need to manage your livestock and your business all in one place. Try it today and take control of your farm or ranch!
                             </p>
-                            <p>Check out Name <a href='https://djurnal.herokuapp.com/' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Check it Out <a href='https://djurnal.herokuapp.com/' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Github code <a href='https://github.com/ONOWONI/poultry-plus.git' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Built with Django, JavaScript, HTML, CSS </p>
                         </div>
                     </div>
                 </FullpageSection>
                 <FullpageSection>
                     <div className="right-card project-card not-first">
                         <div className="project-img">
-                            <img src={poolinc} alt="Home screen of project" />
+                            <img src={img} alt="Home screen of project" />
                         </div>
                         <div className="project-text">
-                            <h1>Poolinc</h1>
-                            <p>This is an two part app that helps the poolinc crew find new customers
-                                the frontend is built with react and it fetches data from a Django rest Framework API on the backend
+                            <h1>Djurnal</h1>
+                            <p>Welcome to our daily journaling website! We understand the importance of taking time to reflect on and document your daily experiences, and our platform makes it easy to do just that. With our user-friendly interface, you can quickly and easily record your thoughts, feelings, and memories from each day. Whether you're looking to track your personal growth, set and achieve goals, or simply have a place to vent and process your emotions, our website is here to help. Sign up now and start documenting your days!"
                             </p>
-                            <p>Check out Poolinc <a href='https://poolinc.netlify.app/' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Check It Out <a href='https://poolinc.netlify.app/' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Github code <a href='https://github.com/ONOWONI/my_blog_post_Flask_web.git' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Built with Flask, HTML, css</p>
                         </div>
                     </div>
                 </FullpageSection>
                 <FullpageSection>
                     <div className="left-card project-card not-first">
                         <div className="project-img">
-                            <img src={poolinc2} alt="Home screen of project" />
+                            <img src={poolinc} alt="Home screen of project" />
                         </div>
                         <div className="project-text">
                             <h1>Poolinc</h1>
-                            <p>This is an two part app that helps the poolinc crew find new customers
-                                the frontend is built with react and it fetches data from a Django rest Framework API on the backend
+                            <p>"Looking for a reliable and professional pool cleaning service? Look no further!
+                                 Our team of experienced and certified pool cleaners is here to help you keep your pool sparkling clean and clear all season long. From regular maintenance and chemical balancing to equipment repairs and replacements, we have the skills and equipment to handle all of your pool care needs. With competitive pricing and flexible scheduling, we make it easy to keep your pool in top shape. Contact us today and let us help you enjoy your pool to the fullest!"
                             </p>
-                            <p>Check out Poolinc <a href='/work' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Check out Poolinc <a href='/work' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Github code <a href='https://github.com/ONOWONI/poolinc.git' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Built with React, CSS</p>
                         </div>
                     </div>
                 </FullpageSection>
