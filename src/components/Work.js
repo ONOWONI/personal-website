dc70b497d4da0fd4146828c8d6a04176963a1457
Onowoni <danielonowoni1@gmail.com>
2023-02-05 15:25:18 +0100

    mobile styling

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,7 @@
-import img from '../img/djurnal.png'
-import poolinc from '../img/poolinc.png'
-import cv from "../img/DanielOnowoni.pdf";
+import img from '../img/poultry-plus.png';
+import img2 from '../img/djurnal.png';
+import poolinc from '../img/poolinc.png';
+import cv from '../img/danielonowoniCV.pdf';
 import { Fullpage, FullPageSections, FullpageSection, FullpageNavigation } from '@ap.cx/react-fullpage'
 
 export default function Work() {
@@ -22,7 +23,7 @@ export default function Work() {
                                 our built-in bookkeeping features allow you to easily track your expenses and income, so you can keep your farm or ranch running smoothly. With our user-friendly interface and powerful features,
                                 you'll have everything you need to manage your livestock and your business all in one place.
                             </p>
-                            <p className='bottom-text'>Check it Out <a href='https://djurnal.herokuapp.com/' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Check it Out <a href='https://poultry-plus.onrender.com' className='out-link' target="_blanc" >here</a></p>
                             <p className='bottom-text'>Github code <a href='https://github.com/ONOWONI/poultry-plus.git' className='out-link' target="_blanc" >here</a></p>
                             <p className='bottom-text'>Built with Django, JavaScript, HTML, CSS </p>
                         </div>
@@ -31,13 +32,13 @@ export default function Work() {
                 <FullpageSection>
                     <div className="right-card project-card not-first">
                         <div className="project-img">
-                            <img src={img} alt="Home screen of project" />
+                            <img src={img2} alt="Home screen of project" />
                         </div>
                         <div className="project-text">
                             <h1>Djurnal</h1>
                             <p>Welcome to our daily journaling website! We understand the importance of taking time to reflect on and document your daily experiences, and our platform makes it easy to do just that. With our user-friendly interface, you can quickly and easily record your thoughts, feelings, and memories from each day. Whether you're looking to track your personal growth, set and achieve goals, or simply have a place to vent and process your emotions, our website is here to help. Sign up now and start documenting your days!"
                             </p>
-                            <p className='bottom-text'>Check It Out <a href='https://poolinc.netlify.app/' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Check It Out <a href='https://djurnal.onrender.com' className='out-link' target="_blanc" >here</a></p>
                             <p className='bottom-text'>Github code <a href='https://github.com/ONOWONI/my_blog_post_Flask_web.git' className='out-link' target="_blanc" >here</a></p>
                             <p className='bottom-text'>Built with Flask, HTML, css</p>
                         </div>
@@ -53,7 +54,7 @@ export default function Work() {
                             <p>"Looking for a reliable and professional pool cleaning service? Look no further!
                                  Our team of experienced and certified pool cleaners is here to help you keep your pool sparkling clean and clear all season long. From regular maintenance and chemical balancing to equipment repairs and replacements, we have the skills and equipment to handle all of your pool care needs. With competitive pricing and flexible scheduling, we make it easy to keep your pool in top shape. Contact us today and let us help you enjoy your pool to the fullest!"
                             </p>
-                            <p className='bottom-text'>Check out Poolinc <a href='/work' className='out-link' target="_blanc" >here</a></p>
+                            <p className='bottom-text'>Check out Poolinc <a href='https://poolinc.netlify.app/' className='out-link' target="_blanc" >here</a></p>
                             <p className='bottom-text'>Github code <a href='https://github.com/ONOWONI/poolinc.git' className='out-link' target="_blanc" >here</a></p>
                             <p className='bottom-text'>Built with React, CSS</p>
                         </div>
